222486f0170a431aad6cd19924c9d591e0712867
theprogmatheus <theprog.matheus@gmail.com>
2022-09-24 15:46:04 -0300

    created default style file, chaned home page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// Default Style
+import './style.scss';
+
+// Router Confi
 import RouterConfig from './config/RouterConfig';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,7 +3,20 @@ import Style from './style.module.scss';
 const HomePage = () => {
   return (
     <div className={Style.container}>
-      HomePage
+
+      <div className={Style.brand}>
+        <h1 className={Style.brandTitle}>Bot Aguiar</h1>
+        <p className={Style.brandDescription}>
+          Bot Aguiar, é um bot de WhatsApp open-source desenvolvido por
+          Matheus Aguiar <a target="_blank" href="https://github.com/theprogmatheus">@theprogmatheus</a>,
+          inicialmente para Desktop (Windows), usando tecnologias como
+          Node, Electron e React.js.
+        </p>
+        <p className={Style.brandDescription}>
+          Para começar a usar o Bot, você precisa escanear o QR-Code ao lado com o seu WhatsApp.
+        </p>
+      </div>
+
     </div>
   )
 }
