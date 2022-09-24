3ad14c11d7eb5403baf53c4af51018e59c7943c3
theprogmatheus <theprog.matheus@gmail.com>
2022-09-24 15:28:22 -0300

    created `HomePage`

--- src/config/RouterConfig.jsx
+++ src/config/RouterConfig.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 // Import Pages
 import NotFoundPage from '../pages/NotFound';
+import HomePage from '../pages/Home';
 
 const RouterConfig = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="*" element={<NotFoundPage />} />
+                <Route path="/" element={<HomePage />} />
             </Routes>
         </BrowserRouter>
     )
